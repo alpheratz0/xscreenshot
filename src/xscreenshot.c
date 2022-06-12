416edcf3a1aa5e64890ad5f37f166ada1bbb30f2
alpheratz0 <alpheratz99@protonmail.com>
2022-06-12 19:44:59 -0300

    misc: add cowthink message

--- src/xscreenshot.c
+++ src/xscreenshot.c
@@ -13,6 +13,18 @@
 	this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 	Place, Suite 330, Boston, MA 02111-1307 USA
 
+	 _______________ 
+	( screenshotubi )
+	 --------------- 
+	  o
+	   o 
+	      /  \~~~/  \
+	     (    ..     )----,
+	      \__     __/      \
+	        )|  /)         |\
+	         | /\  /___\   / ^
+	          "-|__|   |__|
+
 */
 
 #include <stdio.h>

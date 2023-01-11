eb00d6b6407d12fd59a28ee267e5b1437e550d7e
alpheratz0 <alpheratz99@protonmail.com>
2023-01-11 15:01:30 -0300

    change order of linked libraries

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@ VERSION   = 0.3.1
 CC        = cc
 INCS      = -I/usr/X11R6/include
 CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
-LDLIBS    = -lxcb -lpng -lxcb-xfixes
+LDLIBS    = -lxcb -lxcb-xfixes -lpng
 LDFLAGS   = -L/usr/X11R6/lib -s
 
 PREFIX    = /usr/local
